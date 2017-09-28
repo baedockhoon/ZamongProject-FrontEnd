c2b38587ff94c7ae42e97c93471199e686334aab
kosmo13 <kosmo13@kosmo13-PC>
2017-09-28 10:36:52 +0900

    뮤직비디오 dto

--- src/com/zamong/mv/service/MusicVideoDTO.java
+++ src/com/zamong/mv/service/MusicVideoDTO.java
@@ -12,6 +12,7 @@ public class MusicVideoDTO {
 	private String mv_contents;
 	private String mv_link;
 	private String mv_hitcount;
+	private String mv_image;
 	
 	/*	MV_NO
 	MV_REGIDATE
@@ -26,8 +27,9 @@ public class MusicVideoDTO {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public MusicVideoDTO(String mv_no, Date mv_regidate, String at_no, String ss_no, String mv_title,
-			String mv_contents, String mv_link, String mv_hitcount) {
+			String mv_contents, String mv_link, String mv_hitcount, String mv_image) {
 		super();
 		this.mv_no = mv_no;
 		this.mv_regidate = mv_regidate;
@@ -37,55 +39,79 @@ public class MusicVideoDTO {
 		this.mv_contents = mv_contents;
 		this.mv_link = mv_link;
 		this.mv_hitcount = mv_hitcount;
+		this.mv_image = mv_image;
 	}
-	
-	
+
 	public String getMv_no() {
 		return mv_no;
 	}
+
 	public void setMv_no(String mv_no) {
 		this.mv_no = mv_no;
 	}
+
 	public java.sql.Date getMv_regidate() {
 		return mv_regidate;
 	}
+
 	public void setMv_regidate(java.sql.Date mv_regidate) {
 		this.mv_regidate = mv_regidate;
 	}
+
 	public String getAt_no() {
 		return at_no;
 	}
+
 	public void setAt_no(String at_no) {
 		this.at_no = at_no;
 	}
+
 	public String getSs_no() {
 		return ss_no;
 	}
+
 	public void setSs_no(String ss_no) {
 		this.ss_no = ss_no;
 	}
+
 	public String getMv_title() {
 		return mv_title;
 	}
+
 	public void setMv_title(String mv_title) {
 		this.mv_title = mv_title;
 	}
+
 	public String getMv_contents() {
 		return mv_contents;
 	}
+
 	public void setMv_contents(String mv_contents) {
 		this.mv_contents = mv_contents;
 	}
+
 	public String getMv_link() {
 		return mv_link;
 	}
+
 	public void setMv_link(String mv_link) {
 		this.mv_link = mv_link;
 	}
+
 	public String getMv_hitcount() {
 		return mv_hitcount;
 	}
+
 	public void setMv_hitcount(String mv_hitcount) {
 		this.mv_hitcount = mv_hitcount;
 	}
+
+	public String getMv_image() {
+		return mv_image;
+	}
+
+	public void setMv_image(String mv_image) {
+		this.mv_image = mv_image;
+	}
+	
 }
