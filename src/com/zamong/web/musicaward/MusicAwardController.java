4df47c9e4e3a3697479747823f791f74525482b5
kosmo13 <kosmo13@kosmo13-PC>
2017-10-08 14:48:03 +0900

    뮤직어워드 후보DTO 추가 10/8덕훈

--- src/com/zamong/web/musicaward/MusicAwardController.java
+++ src/com/zamong/web/musicaward/MusicAwardController.java
@@ -11,8 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.zamong.ca.service.CandidatesDTO;
+import com.zamong.ca.service.impl.CandidatesServiceImpl;
+import com.zamong.ma.service.MusicAwardDTO;
 import com.zamong.ma.service.impl.MusicAwardServiceImpl;
-import com.zamong.mv.service.MusicVideoDTO;
 
 @Controller
 public class MusicAwardController {
@@ -20,13 +22,23 @@ public class MusicAwardController {
 	@Resource(name="musicAwardServiceImpl")
 	private MusicAwardServiceImpl service;
 	
+	@Resource(name="candidatesServiceImpl")
+	private CandidatesServiceImpl service2;
+	
 	//기본List
 		@RequestMapping("/ZamongFrontEnd/MusicAward.do")
-		public String list() throws Exception{
+		public String list(HttpServletRequest req,//페이징용 메소드에 전달
+				@RequestParam Map map,//검색용 파라미터 받기
+				Model model//리퀘스트 영역 저장용
+				) throws Exception{
 			//서비스 호출]
-			/*List<MusicVideoDTO> list= service.selectList(map);
+			List<MusicAwardDTO> list= service.selectList(map);
+			
+			List<CandidatesDTO> calist= service2.selectList(map);
+			
 			//데이타 저장]		
-			model.addAttribute("list", list);*/
+			model.addAttribute("list", list);
+			model.addAttribute("calist", calist);
 			
 			return "/WEB-INF/bbs/musicAward/MusicAwardMain.jsp";
 		}
