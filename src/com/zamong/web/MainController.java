9462bf8af8aa340e98a3eba5fbef7971f2c25208
admin <admin@kosmo_16>
2017-10-11 01:46:57 +0900

    이수범 동희 요청사항 완료

--- src/com/zamong/web/MainController.java
+++ src/com/zamong/web/MainController.java
@@ -4,25 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.annotations.Param;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.zamong.ch.service.CashDTO;
 import com.zamong.ch.service.impl.CashServiceImpl;
-import com.zamong.me.service.impl.MemberServiceImpl;
 import com.zamong.ss.service.SoundDTO;
 import com.zamong.ss.service.impl.SoundServiceImpl;
-import com.zamong.web.PagingUtil;
-@SessionAttributes("me_id")
+
 @Controller
 public class MainController {
 
@@ -34,13 +26,15 @@ public class MainController {
 	@RequestMapping("/ZamongFrontEnd/main.do")
 	public String mainPage(
 			//@Param("al_divide") String al_divide,
-			Model model,@ModelAttribute("me_id") String me_id,CashDTO dto//리퀘스트 영역 저장용
+			Model model, CashDTO dto//리퀘스트 영역 저장용
+			, HttpSession session
 			) throws Exception{
 		//dto.setAl_divide(al_divide);
-		dto.setMe_id(me_id);
+		String me_id = (String) session.getAttribute("me_id");
 		System.out.println(me_id);
 		if(me_id != null && me_id != "") {
-			cash_service.selectOne(dto);
+			dto.setMe_id(me_id);
+			dto = cash_service.selectOne(dto);
 			model.addAttribute("dto",dto);
 		}
 		List<SoundDTO> list = service.selectMainPage();
